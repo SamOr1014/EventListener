f2798fd8d74100b3695392deaa23f353880f9080
SamOr1014 <orchamsumsam1014@gmail.com>
2022-07-06 16:12:31 +0800

    Added route and static file to the server

--- router/login.ts
+++ router/login.ts
@@ -2,6 +2,11 @@ import express from 'express'
 
 export const login = express.Router()
 
+
+login.get('/', (req, res)=> {
+    res.redirect('signup.html')
+})
+
 login.post('/', (req, res)=> {
     console.log("You posted to route Login")
     res.send("Login")
--- server.ts
+++ server.ts
@@ -1,10 +1,31 @@
 import express from 'express'
+import formidable from 'formidable'
+import fs from 'fs'
 
-//app declare
-const app = express();
 //import routers
 import {login} from './router/login'
 import {register} from './router/register'
+
+const uploadDir = 'uploads'
+fs.mkdirSync(uploadDir, { recursive: true })
+
+export const form = formidable({
+  uploadDir,
+  keepExtensions: true,
+  maxFiles: 1,
+  maxFileSize: 200 * 1024 ** 2, // the default limit is 200KB
+  filter: part => part.mimetype?.startsWith('image/') || false,
+})
+
+//app declare
+const app = express(); 
+
+//Parse Multiform Data
+app.use(express.urlencoded({ extended: true }))
+//Parse Json
+app.use(express.json())
+
+
 
 app.get('/', (req, res)=> {
     res.send("main")
@@ -15,7 +36,9 @@ app.use('/login', login)
 app.use('/register', register)
 
 
-
+app.use(express.static('public'))
+app.use(express.static('member'))
+app.use(express.static('Private'))
 //Listening to Port 8080
 const PORT = 8080
 app.listen(PORT, ()=> {
