9f69c9867ac92ba707752fe3837745fb0c205d1e
yat944 <yat94424@gmail.com>
2022-07-08 16:11:49 +0800

    updated

--- public/event-details.js
+++ public/event-details.js
@@ -4,15 +4,39 @@ document.querySelector("#apply-now").addEventListener("click", function (event) 
 });
 
 window.onload = () => {
-  initEventDetails();
+  loadEventDetails();
 };
 
-async function initEventDetails() {
-  // const form = e.target;
-  const formData = new FormData();
-  formData.append();
-  formData.append();
-  const resp = await fetch("/", {
-    body: formData,
-  });
+async function loadEventDetails() {
+  const resp = await fetch("./event");
+  const events = await resp.json();
+  let htmlStr = "";
+  for (const event of events) {
+    htmlStr += /*html */ `<div class="event-detailsInfo">
+    <div class="event-name">Event Name:</div>
+    <div id="event-content-text">
+      <div class="time">Time:</div>
+      <ul>
+        <li></li>
+      </ul>
+      <div class="venue">Venue</div>
+      <ul>
+        <li></li>
+      </ul>
+      <div class="fee">Fee</div>
+      <ul>
+        <li></li>
+      </ul>
+      <div class="max-pp">Max-participants</div>
+      <ul>
+        <li></li>
+      </ul>
+
+      <div class="description">Description</div>
+    </div>
+  </div>
+</div>
+</div>`;
+  }
+  document.querySelector(".event-detailsInfo").innerHTML = htmlStr;
 }
---
+++ public/event-details.ts
@@ -0,0 +1,9 @@
+import { client } from "../server";
+import { Request, Response } from "express";
+
+async function getAllEvent(req: Request, res: Response) {
+  const getEvent = (
+    await client.query(/*sql */ `SELECT id, name, venue, max_participant, FROM events ORDER BY id`)
+  ).rows;
+  res.json(getEvent);
+}
