6fde9e9edc2f1b1899a9946bfd5a95a649032449
SamOr1014 <orchamsumsam1014@gmail.com>
2022-07-08 16:42:30 +0800

    admin final update on users and events display page

--- private/private-js/admin-allusers.js
+++ private/private-js/admin-allusers.js
@@ -1,11 +1,24 @@
 async function loadAllEventAdmin(){
-    const eventsFromServer = await fetch('/account//userdetail', {
+    const eventsFromServer = await fetch('/account/alluserdetail', {
         method: 'GET'
     })
-    const eventsDetail = await eventsFromServer.json()
-    console.log(eventsDetail);
-    for (let event of eventsDetail){
-        document.querySelector('#showing-panel').innerHTML = ``
+    const usersDetail = await eventsFromServer.json()
+    console.log(usersDetail);
+    for (let user of usersDetail){
+        let statusButton = user.is_banned? "Unban":"Ban"
+        let buttonClass = user.is_banned? "danger":"primary"
+        document.querySelector('#showing-panel').innerHTML = `<div class="col-md-3 user-cards" userid="">
+        <div class="card">
+          <div class="card-body">
+            <p class="card-title">Name: ${user.first_name + " " + user.last_name}</p>
+            <p class="card-text">Email: ${user.email}</p>
+            <p class="card-text">Phone: ${user.phone}</p>
+            <p class="card-text">User ID: ${user.id}</p>
+            <a id="${statusButton}-button" class="btn btn-${buttonClass}">${statusButton}</a>
+          </div>
+        </div>
+    </div>
+    `
     }
 }
 
@@ -14,5 +27,5 @@ loadAllEventAdmin()
 
 
 
-console.log('loading all event')
+console.log('loading all users')
 
