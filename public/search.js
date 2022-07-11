40ec604b0cb9f2c6dc3786b78d50c9e6db1bf1d5
SamOr1014 <orchamsumsam1014@gmail.com>
2022-07-11 12:18:44 +0800

    search.js update and headers update

--- public/search.js
+++ public/search.js
@@ -1,26 +1,4 @@
-
-// async function getData() {
-//     const category = localStorage.getItem("category")
-//     const url = `./.......?category=${category}`
-
-//     document.querySelector("#result-type").innerHTML = category
-
-//     // method: "GET"
-//     // backend >> req.query
-//     const dataJson = await fetch(url)
-//     const data = await data.json()
-
-//     // manage html
-//     const dataBoard = document.querySelector("#content")
-//     const htmlStr = `<div> ${data.name}, ${data.image}, ${data.venue}, ${data.date}, ${data.user}, ${data.rating} </div>`
-//     dataBoard.innerHTML = htmlStr
-// } 
-
-// // getData()      
-
-
-// // Result: image, event name, event venue, event date, user(organizer) bio, rating
-
-
-
-
+window.onload = () => {
+    const genre = window.location.search
+    console.log(genre)
+  }
