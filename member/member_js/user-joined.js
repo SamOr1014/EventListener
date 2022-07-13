fed226cc40ec8069ff896d381ccb528a668b2ed5
SamOr1014 <orchamsumsam1014@gmail.com>
2022-07-13 14:29:02 +0800

    css update

--- member/member_js/user-joined.js
+++ member/member_js/user-joined.js
@@ -9,46 +9,53 @@ async function loadJoinedEvent() {
     const finalDate =
       realBDay.getFullYear().toString() +
       "-" +
-      (realBDay.getMonth() + 1).toString() +
+      ("0" + (realBDay.getMonth() + 1).toString()).substring(-2) +
       "-" +
-      realBDay.getDate().toString()
+      ("0" + realBDay.getDate().toString()).substring(-2)
+    const finalTime =
+      ("0" + realBDay.getHours().toString()).substring(-2) +
+      ":" +
+      ("0" + realBDay.getMinutes().toString()).substring(-2)
 
-      if (event.type === "sport") {
-        defaulePath = "sports.jpg"
-      } else if (event.type === "board_game") {
-        defaulePath = "board-game.jpg"
-      } else if (event.type === "water_activity") {
-        defaulePath = "water.jpg"
-      } else if (event.type === "gambling") {
-        defaulePath = "gambling.jpg"
-      } else if (event.type === "party") {
-        defaulePath = "party.jpg"
-      } else if (event.type === "workshop") {
-        defaulePath = "workshop.jpg"
-      } else if (event.type === "online_activity") {
-        defaulePath = "online.jpg"
-      } else {
-        defaulePath = "others.jpg"
-      }
-      const image = event.image ? `/${event.image}` : `/${defaulePath}`
+    if (event.type === "sport") {
+      defaulePath = "sports.jpg"
+    } else if (event.type === "board_game") {
+      defaulePath = "board-game.jpg"
+    } else if (event.type === "water_activity") {
+      defaulePath = "water.jpg"
+    } else if (event.type === "gambling") {
+      defaulePath = "gambling.jpg"
+    } else if (event.type === "party") {
+      defaulePath = "party.jpg"
+    } else if (event.type === "workshop") {
+      defaulePath = "workshop.jpg"
+    } else if (event.type === "online_activity") {
+      defaulePath = "online.jpg"
+    } else {
+      defaulePath = "others.jpg"
+    }
+    const image = event.image ? `/${event.image}` : `/${defaulePath}`
+
     document.querySelector(
       "#content"
     ).innerHTML += `<div id="account-panel" class="container-fluid d-flex mt-4 flex-column justify-content-center align-content-center">
     <div id="joined-event-col" class="col-lg-12 d-flex flex-wrap justify-content-around">
-      <div class="col-md-12"><h3>${event.name}</h3></div>
+      <div class="col-md-12 text-center"><h3>${event.name}</h3></div>
       <div class="col-md-8 p-2">
           <img src="${image}" class="w-100 h-100">
       </div>
       
       <div class="col-md-4 p-2">
-          <div><h4>Venue: </h4></div>
-          <div><h5>@ ${event.venue}</h5></div>
-          <div><h4>Date: </h4></div>
-          <div><h5>on ${finalDate}</h5></div>
-          <div><h4>Event Bio:</h4></div>
-          <div class="col-md-4 p-2 w-100">
-              <div id="bio-text">${event.bio}</div>
-          </div>
+      <div><h4>Venue: </h4></div>
+      <div><h5>@ ${event.venue}</h5></div>
+      <div><h4>Date: </h4></div>
+      <div><h5>on ${finalDate}</h5></div>
+      <div><h4>Time: </h4></div>
+      <div><h5>at ${finalTime}</h5></div>
+      <div><h4>Event Bio:</h4></div>
+      <div class="col-md-4 p-2 w-100">
+          <div id="bio-text">${event.bio}</div>
+      </div>
       </div>
     </div>
   </div>
--- public/event-details.js
+++ public/event-details.js
@@ -95,19 +95,19 @@ async function userProfileInEventDetails(eventid) {
   console.log(userInfo)
   let image = userInfo.profile_img ? userInfo.profile_img : "/profile-pic.jpg"
   htmlProfileCard.innerHTML = `
-  <a href="#">
+  <div id="profile-img">
     <img
       src="${image}"
       class="card-img-top w-100 rounded-circle"
       alt="..."
     />
-  </a>
-  <div class="card-body">
+  </div>
+  <div class="card-body w-100">
     <div><h5 class="card-title">Name:${userInfo.first_name + " " + userInfo.last_name}</h5></div>
     <p class="card-text">Contact:${userInfo.phone}</p>
     <p class="card-text">Email : ${userInfo.email}</p>
     <p class="card-text">Bio : ${userInfo.bio}</p>
-    <button class="btn btn-info" id="follow-btn">Follow</button>
+    <button class="btn btn-info mt-2" id="follow-btn">Follow</button>
   </div>
 `
   // console.log(htmlProfileCard)
@@ -268,7 +268,6 @@ async function addComment(eventid) {
 
       html += `<div class="d-flex flex-column">
     <div id ="user"> 
-
     <p>    <img
     src="${image}"
     alt=""
